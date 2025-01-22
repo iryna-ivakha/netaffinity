26352f3ca9415fd665ee0ca6f8c400dbb5509584
iryna.ivakha <iryna.ivakha@intive.com>
2025-01-22 22:19:55 +0200

    R;2Cefactor to remove primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,11 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
-    provideHttpClient(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
+    provideHttpClient(withFetch())
   ]
 };
--- src/app/vidget/vidget.component.ts
+++ src/app/vidget/vidget.component.ts
@@ -1,20 +1,26 @@
-import { Component, Input } from '@angular/core';
-import { ChartModule } from 'primeng/chart';
+import { Component, Input, ViewChild, ElementRef, signal, Signal, effect } from '@angular/core';
 import { ChartDatagroup } from '../models/data-models';
-import { ChartType } from 'chart.js';
+import { ChartType, ChartData, ChartOptions } from 'chart.js';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-vidget',
-  imports: [ChartModule],
   templateUrl: './vidget.component.html',
-  styleUrl: './vidget.component.scss'
+  styleUrls: ['./vidget.component.scss']
 })
 
-export class VidgetComponent {
+export class VidgetComponent {  
+  dataReady$ = signal('');
+  canvasReady$ = signal('');
   type: ChartType = 'line';
   title: string = 'Vidget';
-  options: unknown[] = [];
-  data: unknown[] = [];
+  options: ChartOptions = {};
+  data: ChartData = {
+    labels: [],
+    datasets: []
+  };
+  chart: any;
+  @ViewChild('VidgetChart') vidgetChart!: ElementRef;
 
   @Input() set chartData(value: ChartDatagroup | undefined) {
     if (value && value.datasets && value.options && value.type && value.title) {
@@ -22,11 +28,39 @@ export class VidgetComponent {
       this.options = value.options;
       this.type = value.type;
       this.title = value.title;
+      this.dataReady$.set('dataReady');  
     }
-  };
-
-  ngOnInit() {
-
   }
-}
 
+  constructor() {
+    effect(() => {
+      if (this.dataReady$() && this.canvasReady$()) {
+        this.createChart();
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.canvasReady$.set('canvasReady');
+  }
+  
+  ngAfterViewChecked() {
+    if (this.dataReady$() && this.canvasReady$()) {
+      this.createChart();
+    }
+  }
+
+
+  createChart(){
+    const chartCanvas = this.vidgetChart.nativeElement;
+    try { 
+      this.chart = new Chart(chartCanvas, {
+        type: this.type,
+        data: this.data,
+        options: this.options
+      });
+    } catch (error) {
+      console.error('Error creating chart:', error);
+    };
+  }
+}  
